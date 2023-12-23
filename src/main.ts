88673ec1639a9b5692c7eb3a5f1f920a1eae7b5b
Israel <isrovick@gmail.com>
2023-12-22 23:08:37 -0400

    Feat: 🦾 Creating Tmp folder (only for test purposes)
    - Creating tmp to assure that all files can be written or read from this folder.
    
    Ref:

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as pkg from '../package.json';
+import * as fs from 'fs';
+const tmpPath = `${process.cwd()}/tmp`;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
+
+  if (!fs.existsSync(tmpPath)) fs.mkdirSync(tmpPath);
+
   console.log(`------------------- RealOne Designli  ----------------------`);
   console.log(`API service (${pkg.name}:v${pkg.version})`);
   console.log(`Running on: ${await app.getUrl()}`);
